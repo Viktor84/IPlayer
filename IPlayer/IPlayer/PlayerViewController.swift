f1e7d6fade8b81099e7ea6bfe8ba9695fda6bc99
viktor <viktorzp84@gmail.com>
2018-05-10 19:44:20 +0300

    add player

--- IPlayer/IPlayer/PlayerViewController.swift
+++ IPlayer/IPlayer/PlayerViewController.swift
@@ -8,17 +8,42 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
     //@IBOutlet weak var dataLabel: UILabel!
     var dataObject: UIViewController?  // dataObject ?
     
-    
+    var player = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            if let auduoPath = Bundle.main.path(forResource: "daft-punk-robot-rock", ofType: "mp3") {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: auduoPath))
+            }
+        } catch {
+            print ("ERROR")
+        }
+        //self.player.play()
     }
+    
+    //MARK: - ACtion
+    @IBAction func playButton(_ sender: Any) {
+        self.player.play()
+    }
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        self.player.pause()
+    }
+    
+    
+    
+    
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
